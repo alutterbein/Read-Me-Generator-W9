51412fde49fb2127799dc881a40aa5eafceaec0e
alutterbein <alutterbein@gmail.com>
2024-06-21 15:28:04 -0700

    fixing repo

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
 const fs=require('fs')
+const path = require('path')
 const generateMarkdown = require('./utils/generateMarkdown') 
 // TODO: Create an array of questions for user input
-const questions = [
+inquirer
+    .prompt([
     {
         type: 'input',
         message: 'give a title to your readme',
@@ -11,10 +13,36 @@ const questions = [
     },
     {
         type: 'input',
-        message: 'describe your readme',
-        name: 'title'
+        message: 'Give us the why, what, how of your project',
+        name: 'description'
     },
-];
+    {
+        type: 'input',
+        message: 'Create a table of contents for your readme',
+        name: 'ToC'
+    },
+    {
+        type: 'input',
+        message: 'Give instalation instructions for your project',
+        name: 'Installation'
+    },
+    {
+        type: 'input',
+        message: 'How do we use your project?',
+        name: 'Usage'
+    },
+    {
+        type: 'input',
+        message: 'describe your readme',
+        name: 'Credits'
+    },
+    {
+        type: 'input',
+        message: 'describe your readme',
+        name: 'License'
+    },
+
+]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
