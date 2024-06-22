ab8153a191a310285c35a4e5d8f2dcdab4f20d77
alutterbein <alutterbein@gmail.com>
2024-06-21 17:21:50 -0700

    final code

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer')
 const fs=require('fs')
 const path = require('path')
 const generateMarkdown = require('./utils/generateMarkdown') 
-// TODO: Create an array of questions for user input
+// array of questions for user input
 const questions = [
     {
         type: 'input',
@@ -54,12 +53,12 @@ const questions = [
 
 ];
 
-// TODO: Create a function to write README file
+// function to write README file
 function writeToFile(fileName, data) {
     return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 
 }
-// TODO: Create a function to initialize app
+// function to initialize app
 function init() {
     inquirer
     .prompt(questions)
